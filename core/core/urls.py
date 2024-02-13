8857ca140531b79c650155d3ade5aa10f4c435e9
Desi-Saitama <devofficial0785@gmail.com>
2024-02-13 19:00:42 +0530

    Registration method is created

--- core/core/urls.py
+++ core/core/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('delete-receipe/<id>/', delete_receipe, name='delete_receipe'),
     path('update-receipe/<id>/', update_receipe, name='update_receipe'),
+    path('login/', login_page, name="login_page"),
+    path('register/', register, name="register_page"),
 ]
 
 if settings.DEBUG:
