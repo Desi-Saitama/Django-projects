c396c08131c9a9561dec719b94138022ea4ee70b
Desi-Saitama <devofficial0785@gmail.com>
2024-02-14 22:10:53 +0530

    login added in the vege app

--- core/vege/views.py
+++ core/vege/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def receipes(request):
@@ -58,6 +59,24 @@ def update_receipe(request, id):
 
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.error(request, 'Invalid Password')
+            return redirect('/login/')
+        
+        else:
+            login(request, user)
+            return redirect('/receipes/')
+
     return render(request, 'login.html')
 
 def register(request):
